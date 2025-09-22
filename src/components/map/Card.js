e2f818afd340c3fc9a5b8c46d1386c578ab67640
Vega Feng <vega.feng@oocl.com>
2025-09-22 14:55:46 +0800

    feat(#11): add card to display day

---
+++ src/components/map/Card.js
@@ -0,0 +1,14 @@
+// Card.js
+import React from 'react';
+import './Card.css'; // 导入样式文件
+
+const Card = ({ name, day }) => {
+    return (
+        <div className="card">
+            <h4>{name}</h4>
+            <p>第{day}天</p>
+        </div>
+    );
+};
+
+export default Card;
