fd262761050d68347ae230ef1464975b6d59d8db
lyle.li <lily7@oocl.com>
2025-09-22 11:48:54 +0800

    refactor: turn to js

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Map from "./components/Map";
+
+function App() {
+  return (
+    <div className="App">
+      <main style={{ padding: '1rem', maxWidth: 960, margin: '0 auto' }}>
+        <Map />
+      </main>
+    </div>
+  );
+}
+
+export default App;
