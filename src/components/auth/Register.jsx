18d64f3b5f7ff4a3a7c6fb9a56b3a49692c517b3
vegafeng <vega.feng@oocl.com>
2025-09-24 16:45:03 +0800

    feat(#27): add register logic and connect backend api

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './css/Register.css';
 import logo from '../../assets/logo192.png';
+import { register as registerApi } from '@/components/apis/api';
+import { message } from 'antd';
+import { useNavigate } from 'react-router';
 
 const Register = () => {
   const [phone, setPhone] = useState('');
@@ -8,17 +11,51 @@ const Register = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
 
   // 表单验证（留空，供后续填写）
   const validateForm = () => {
     // 后续自行添加验证逻辑
+    // 验证输入发手机号是否为11个数字
+    if (!/^\d{11}$/.test(phone)) {
+      setError('请输入有效的手机号');
+      return false;
+    }
+    // 验证密码是否为6-16个字符
+    if (!/^\w{6,16}$/.test(password)) {
+      setError('请输入有效的密码');
+      return false;
+    }
     return true;
   };
 
   // 注册处理（留空，供后续填写）
-  const handleRegister = (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
-    // 后续自行添加注册逻辑
+    if (!validateForm()) {
+      return;
+    }
+    setIsLoading(true);
+    try {
+      // 发送注册请求
+      const response = await registerApi({ "phone": phone, "password": password });
+      if (response.status === 201) {
+        setIsLoading(false);
+        // 注册成功，保存手机号和提示到localStorage
+        localStorage.setItem('registerPhone', phone);
+        message.success('注册成功，请登录');
+        // localStorage.setItem('registerSuccessMsg', '注册成功，请登录');
+        navigate('/login');
+      }
+    } catch (err) {
+      if (err.response && err.response.status === 409) {
+        setError('手机号已被注册，请直接登录');
+      }else if (err.response && err.response.status === 400) {
+        setError('手机号或密码格式错误，请重新输入');
+      }
+      setError('注册失败，请重试');
+      setIsLoading(false);
+    }
   };
 
   return (
